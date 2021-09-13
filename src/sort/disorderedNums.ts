3d9cdf936716c8f676dfde00f882f90d277bc61b
Neo <thrulife2gether@foxmail.com>
2021-09-13 13:23:39 +0800

    fix: 修复选择排序的bug

--- src/sort/disorderedNums.ts
+++ src/sort/disorderedNums.ts
@@ -1,1 +1,4 @@
-export const disorderedNum = [5, 4, 3, 2, 1];
+// export const disorderedNum = [5, 4, 3, 2, 1];
+export const disorderedNum = [
+  3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48,
+];
--- src/sort/selectionSort.ts
+++ src/sort/selectionSort.ts
@@ -4,15 +4,17 @@ export function selectionSort(numbers: number[]) {
   for (let i = 0; i < nums.length - 1; i++) {
     let iMin = i;
 
-    for (let j = i; j < nums.length - i; j++) {
+    for (let j = i; j < nums.length; j++) {
       if (nums[j]! < nums[iMin]!) {
         iMin = j;
       }
     }
 
-    const temp = nums[i]!;
-    nums[i] = nums[iMin]!;
-    nums[iMin] = temp;
+    if (iMin !== i) {
+      const temp = nums[i]!;
+      nums[i] = nums[iMin]!;
+      nums[iMin] = temp;
+    }
   }
 
   return nums;
